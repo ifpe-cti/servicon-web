ec7bee6a9996d949eecd43f716acd019f5496753
Sebastião Izidorio <sebastian.izydorio@gmail.com>
2018-04-08 22:59:25 -0300

    Criação classe Profissional

--- src/main/java/br/edu/ifpe/servicon/model/entidades/Profissional.java
+++ src/main/java/br/edu/ifpe/servicon/model/entidades/Profissional.java
@@ -21,10 +21,129 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 package br.edu.ifpe.servicon.model.entidades;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 /**
  *
  * @author Lucas Mendes <lucas.mendes147@live.com>
  */
 public class Profissional {
+    
+    private int id_profissional;
+    private String nome;
+    private BigDecimal avaliacao;
+    private String telefone;
+    private String email;
+    private String dataNascimento;
+
+    public Profissional(int id_profissional, String nome, BigDecimal avaliacao, String telefone, String email, String dataNascimento) {
+        this.id_profissional = id_profissional;
+        this.nome = nome;
+        this.avaliacao = avaliacao;
+        this.telefone = telefone;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getId_profissional() {
+        return id_profissional;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getAvaliacao() {
+        return avaliacao;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setId_profissional(int id_profissional) {
+        this.id_profissional = id_profissional;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAvaliacao(BigDecimal avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + this.id_profissional;
+        hash = 53 * hash + Objects.hashCode(this.nome);
+        hash = 53 * hash + Objects.hashCode(this.avaliacao);
+        hash = 53 * hash + Objects.hashCode(this.telefone);
+        hash = 53 * hash + Objects.hashCode(this.email);
+        hash = 53 * hash + Objects.hashCode(this.dataNascimento);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Profissional other = (Profissional) obj;
+        if (this.id_profissional != other.id_profissional) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.telefone, other.telefone)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.dataNascimento, other.dataNascimento)) {
+            return false;
+        }
+        if (!Objects.equals(this.avaliacao, other.avaliacao)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Profissional{" + "id_profissional=" + id_profissional + ", nome=" + nome + ", avaliacao=" + avaliacao + ", telefone=" + telefone + ", email=" + email + ", dataNascimento=" + dataNascimento + '}';
+    }
+    
+    
     
 }
